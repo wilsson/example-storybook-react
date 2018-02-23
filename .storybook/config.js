1d42ff64772bdb3f3e426540ec5e7882bb7fbd25
wilsson <wilsonft83@gmail.com>
2018-02-23 00:55:57 -0500

    feat(storybook-addon-option): add addon-options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  addonPanelInRight: true
+});
 
 function loadStories() {
   require('../stories');
